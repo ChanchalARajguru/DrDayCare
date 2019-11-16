f4042ee9c2015bd8176f1a34b85e3991ef350b8e
a00268815-arvin <a00268815@student.ait.ie>
2019-11-16 12:30:14 +0000

    added adminManager file for method action and UserListResponse Pojo class

---
+++ src/main/java/eu/teama/drdaycare/admin/AdminManager.java
@@ -0,0 +1,47 @@
+package eu.teama.drdaycare.admin;
+
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import eu.teama.drdaycare.DatabaseHandler.DatabaseController;
+import eu.teama.drdaycare.Prescription.PrescriptionManager;
+import eu.teama.drdaycare.Prescription.jsonData.PrescriptionRequest;
+import eu.teama.drdaycare.Prescription.jsonData.PrescriptionResponse;
+import eu.teama.drdaycare.UserTypes.Prescription;
+import eu.teama.drdaycare.UserTypes.User;
+
+@Service
+public class AdminManager {
+
+	Logger logger = LoggerFactory.getLogger(AdminManager.class);
+
+	
+	@Autowired
+    private final DatabaseController databaseController;
+
+    public AdminManager( final DatabaseController databaseController) {this.databaseController = databaseController;}
+
+    public UserListResponse getAllUsers() {
+        Iterable<User> users = databaseController.getAllUsers();
+        ArrayList<User> list  = new ArrayList();
+        for (User user : users) {
+        	list.add(user);
+        }
+        User user = new User();
+    	user.setName("John");
+    	list.add(user);
+        logger.info("Could not find Users");
+        return new UserListResponse (true , list);
+    }
+    
+    public void addUser() {
+    	User user = new User();
+    	user.setName("John");
+    	databaseController.insertUser(user);
+    }
+}
---
+++ src/main/java/eu/teama/drdaycare/admin/UserListResponse.java
@@ -0,0 +1,22 @@
+package eu.teama.drdaycare.admin;
+
+import java.util.ArrayList;
+
+import eu.teama.drdaycare.UserTypes.User;
+
+public class UserListResponse {
+
+	private final ArrayList<User> list;
+    private final boolean valid;
+    public UserListResponse(boolean valid , ArrayList<User> list) {
+        this.valid = valid;
+        this.list = list;
+
+    }
+    public boolean isValid() {
+        return valid;
+    }
+    public ArrayList getList() {
+        return list;
+    }
+}
